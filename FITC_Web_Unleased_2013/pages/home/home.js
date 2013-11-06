ce725fbf36898d1882eef09f6cf5cf78f7eb4e0b
Eric Marthinsen <emarthinsen@agilecommerce.com>
2013-11-05 22:50:42 -0500

    Integrate the demo slideshow into the app. Note, the slide regarding PDF export had to be removed. It does not work with Win8.

--- FITC_Web_Unleased_2013/pages/home/home.js
+++ FITC_Web_Unleased_2013/pages/home/home.js
@@ -1,11 +1,32 @@
 ﻿(function () {
-    "use strict";
+	"use strict";
 
-    WinJS.UI.Pages.define("/pages/home/home.html", {
-        // This function is called whenever a user navigates to this page. It
-        // populates the page elements with the app's data.
-        ready: function (element, options) {
-            // TODO: Initialize the page here.
-        }
-    });
+	WinJS.UI.Pages.define("/pages/home/home.html", {
+		// This function is called whenever a user navigates to this page. It
+		// populates the page elements with the app's data.
+		ready: function (element, options) {
+			Reveal.initialize({
+				controls: true,
+				progress: true,
+				history: true,
+				center: true,
+
+				// Optional libraries used to extend on reveal.js
+				dependencies: [
+					{ src: '/vendor/reveal/lib/js/classList.js', condition: function () { return !document.body.classList; } },
+					{ src: '/vendor/reveal/plugin/markdown/marked.js', condition: function () { return !!document.querySelector('[data-markdown]'); } },
+					{ src: '/vendor/reveal/plugin/markdown/markdown.js', condition: function () { return !!document.querySelector('[data-markdown]'); } },
+					{ src: '/vendor/reveal/plugin/highlight/highlight.js', async: true, callback: function () { hljs.initHighlightingOnLoad(); } },
+					{ src: '/vendor/reveal/plugin/zoom-js/zoom.js', async: true, condition: function () { return !!document.body.classList; } },
+					{ src: '/vendor/reveal/plugin/notes/notes.js', async: true, condition: function () { return !!document.body.classList; } }
+				]
+			});
+
+			Reveal.addEventListener('ready', function (event) {
+				//This is a slight hack to enable highlighting. The reveal.js plugin attaches highlighting to the load event,
+				//but that doesn't work in this case, since we are working with the equivalent of a single-page web app.
+				hljs.initHighlighting();
+			});
+		}
+	});
 })();
